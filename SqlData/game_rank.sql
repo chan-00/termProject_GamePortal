13e58a1b11b74a21b7d9ad1643af03eed72e6b6d
ghostdog <87572532+ghostclog@users.noreply.github.com>
2022-12-01 10:13:45 +0900

    Update game_rank.sql
    데이터갱신

--- SqlData/game_rank.sql
+++ SqlData/game_rank.sql
@@ -1,10 +1,12 @@
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(1,"DJMAX RESPECT V","https://cdn.cloudflare.steamstatic.com/steam/apps/960170/capsule_231x87.jpg?t=1666858912");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(2,"CALL OF DUTY: MODERN WARFARE II","https://cdn.cloudflare.steamstatic.com/steam/apps/1938090/capsule_231x87_koreana.jpg?t=1668813907");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(3,"PUBG: BATTLEGROUNDS","https://cdn.cloudflare.steamstatic.com/steam/apps/578080/capsule_231x87.jpg?t=1667814365");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(4,"Dave The Diver","https://cdn.cloudflare.steamstatic.com/steam/apps/1868140/capsule_231x87.jpg?t=1668065344");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(5,"Football Manager 2023","https://cdn.cloudflare.steamstatic.com/steam/apps/1904540/capsule_231x87.jpg?t=1668082258");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(6,"Sid Meier’s Civilization VI","https://cdn.cloudflare.steamstatic.com/steam/apps/289070/capsule_231x87.jpg?t=1669055236");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(7,"Marvel’s Spider-Man Remastered","https://cdn.cloudflare.steamstatic.com/steam/apps/1817190/capsule_231x87.jpg?t=1668787110");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(8,"MONSTER HUNTER RISE","https://cdn.cloudflare.steamstatic.com/steam/apps/1446780/capsule_231x87.jpg?t=1669252485");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(9,"Warhammer 40,000: Darktide","https://cdn.cloudflare.steamstatic.com/steam/apps/1361210/capsule_231x87.jpg?t=1669118961");
-insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(10,"Sekiro: Shadows Die Twice","https://cdn.cloudflare.steamstatic.com/steam/apps/814380/capsule_231x87.jpg?t=1669076148");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(1,"PUBG: BATTLEGROUNDS","https://cdn.cloudflare.steamstatic.com/steam/apps/578080/capsule_231x87.jpg?t=1667814365");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(2,"Project Zomboid","https://cdn.cloudflare.steamstatic.com/steam/apps/108600/capsule_231x87.jpg?t=1667832656");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(3,"Red Dead Redemption 2","https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/capsule_231x87.jpg?t=1656615305");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(4,"Cyberpunk 2077","https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/capsule_231x87.jpg?t=1663663573");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(5,"EA SPORTS FIFA 23","https://cdn.cloudflare.steamstatic.com/steam/apps/1811260/capsule_231x87_alt_assets_0.jpg?t=1669311247");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(6,"CALL OF DUTY: MODERN WARFARE II","https://cdn.cloudflare.steamstatic.com/steam/apps/1938090/capsule_231x87_koreana.jpg?t=1668813907");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(7,"DJMAX RESPECT V","https://cdn.cloudflare.steamstatic.com/steam/apps/960170/capsule_231x87.jpg?t=1666858912");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(8,"Persona 5 Royal","https://cdn.cloudflare.steamstatic.com/steam/apps/1687950/capsule_231x87.jpg?t=1666698742");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(9,"Marvel’s Spider-Man Remastered","https://cdn.cloudflare.steamstatic.com/steam/apps/1817070/capsule_231x87.jpg?t=1667406675");
+insert into game_rank_list(game_rank, GAME_ENG_NAME, GAME_IMAGE) values(10,"Sid Meier’s Civilization VI","https://cdn.cloudflare.steamstatic.com/steam/apps/289070/capsule_231x87.jpg?t=1669055236");
+
+select * from game_rank_list;
