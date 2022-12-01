fd47d85f51e6c950abb5fb67bc6e3d6ff95dc844
ghostdog <87572532+ghostclog@users.noreply.github.com>
2022-12-01 10:51:01 +0900

    Add files via upload
    갱신 순위에 따른 정보 추가

---
+++ SqlData/GameData3.sql
@@ -0,0 +1,63 @@
+insert into game(GMAE_INFO,CODE_TYPE,PUBLSH_DATE,KOR_NAME,ENG_NAME,MIN_SPEC,SEPC,DEFAULT_PRICE,DEVELOPER,PUBLISHER) values("
+
+게임에 대해
+프로젝트좀보이드(Project Zomboid)는 좀비가 득실거리는 오픈 엔드 샌드박스이다. 그것은 한 가지 간단한 질문을 던집니다: 당신은 어떻게 죽을 것인가? 멀드로와 웨스트포인트의 마을에서 생존자들은 집을 약탈하고, 방어 시설을 짓고, 그들의 피할 수 없는 죽음을 하루하루 늦추기 위해 최선을 다해야 합니다. 어떤 도움도 오지 않는다 – 그들의 지속적인 생존은 그들 자신의 교활함, 행운, 그리고 무자비한 무리를 피할 수 있는 능력에 달려 있다.현실적인 생존에 초점을 맞춘 하드코어 샌드박스 좀비 서바이벌 게임이 현재 특징이다.영구적인 플레이어 실행 서버를 사용한 온라인 멀티 플레이어 서바이벌. 로컬 4 플레이어 분할 화면 공동 작업 군집 역학과 심층 시각 및 청각 시스템을 갖춘 수백 개의 좀비. 전체 시야 시스템 및 실시간 조명, 소리 및 가시성 메커니즘. 그늘에 숨거나, 조용히 하고, 밤에 불을 끄거나, 적어도 창문에 시트를 걸어 놓으세요.   요새를 탐험하고 약탈하고 설치할 수 있는 방대하고 성장하는 지도(실제 세계 위치를 기준으로 느슨하게)입니다. Blindcoder의 지도 프로젝트를 확인해보세요: https://map.projectzomboid.com/ 완전한 물리학과 깊고 사실적인 게임 플레이 메커니즘을 갖춘 차량.무기를 만들고, 바리케이드를 치고, 요리하기 위해 도구와 물건을 사용하세요. 여러분은 심지어 나무를 자르고, 나무를 톱질하고, 보급품을 청소함으로써 좀비 방지 요새를 지을 수 있습니다. 생존을 위해 노력하는 동안 우울증, 지루함, 배고픔, 갈증, 질병에 대처하라. 낮이 밤으로 바뀐다. 전기가 휘청거린다 무리가 이주하다. 겨울이 다가온다. 자연이 점차 지배하기 시작한다.농업, 어업, 목공, 요리, 트래핑, 캐릭터 커스터마이징, 스킬 및 특전은 게임 내에서 수행하는 작업에 따라 개발됩니다. 제대로 작동하지 않는 좀비들. (샌드박스 메뉴에서 하라고 하지 않는 한)자크 비버라는 천재가 만든 엄청난 분위기의 음악 트랙. 상상력 넘치는 챌린지 시나리오와 순간 액션 '라스트 스탠드' 모드, 일반 샌드박스와 서바이벌 풀 위에 오픈하고 강력한 루아모딩 지원. Windows에서 Xbox 컨트롤러 Gamepad 지원. [기타 패드는 수동으로 설정할 수 있습니다. 현재 Mac에서는 게임 패드 지원을 이용할 수 없습니다]저희는 현재 소규모 팀이지만 다음과 같은 기능을 제공하기 위해 노력하고 있습니다.최초의 튜토리얼 역할을 하는 PZ Stories 모드의 복귀는 매 고비마다 적극적으로 당신을 죽이려 합니다. 케이트와 볼드스팟이 돌아왔다!지속적인 세계에서 구동되는 심층적이고 다양한 NPC 만남, 각 플레이 스루를 새로운 내러티브 게임플레이와 함께 당신만의 좀비 서바이벌 영화로 바꾸는 메타게임 시스템에 의해 구동된다.멀드로와 웨스트포인트 주변의 시골과 도시의 지속적인 확장 완전한 야생 생존 시스템, 동물 및 먹이 사냥. 더 많은 아이템, 제작 레시피, 무기 및 게임 플레이 시스템. 스팀 워크샵 및 성과 지원게임에 대한 자세한 내용은 @indiestone을 팔로우하거나 http://www.projectzomboid.com를 방문하십시오.
+",
+" 생존 좀비 오픈 월드 오픈 월드 생존 제작 멀티플레이어 샌드박스 포스트아포칼립스 협동 크래프팅 인디 시뮬레이션 건설 RPG 생존 공포 현실적 등각 싱글 플레이어 앞서 해보기 2D 어드벤처",
+"2013-11-8","프로잭트 좀보이드","Project Zomboid",
+"없음.",
+" OS: Windows 10, 64 Bit   64bit OS required  Processor: Intel 2.77GHz Quad-core  Memory: 8Gb Ram  Hard Disk Space: 5gig   Video Card:  Dedicated graphics card with 2 GB of RAM minimum, OpenGL 2.1 and GLSL 1.2 support (generally 2012 or newer)  Sound: FMOD compatible sound card",20500,50,54);
+
+
+insert into game(GMAE_INFO,CODE_TYPE,PUBLSH_DATE,KOR_NAME,ENG_NAME,MIN_SPEC,SEPC,DEFAULT_PRICE,DEVELOPER,PUBLISHER) values("
+
+게임에 대해
+1899년 미국.
+
+아서 모건과 반 더 린드 갱단은 도주 중인 무법자입니다. 정부 요원과 일류 현상금 사냥꾼들에게 추격당하는 그들은 살아남기 위해 강도질과 도둑질, 싸움을 거듭하며 미국의 험난한 심장부를 달려 나갑니다. 심해져 가는 내부 갈등으로 갱이 해체될 위기 속에서, 아서는 자기를 키워 준 갱에 대한 의리와 자신의 이상 중 하나를 선택해야만 합니다.
+
+이제 추가 스토리 모드 콘텐츠와 사진 모드를 완벽히 즐길 수 있는 Red Dead Redemption 2에서 모두가 함께 즐길 수 있는 생생한 세계인 Red Dead 온라인을 무료로 플레이할 수 있습니다. 플레이어는 여러 개의 직업을 맡아 개척지에서 자신만의 독특한 길을 개척해나갈 수 있습니다. 현상금 사냥꾼이 되어 범죄자를 추적할 수 있고, 상인이 되어 사업을 운영할 수 있습니다. 아니면 수집가가 되어 이국적인 보물을 발굴하거나 밀주업자가 되어 비밀스러운 양조장을 운영하는 등 다양한 활동을 할 수 있습니다.
+
+더욱 깊이 몰입할 수 있도록 완전히 새롭게 향상된 그래픽과 기술적인 요소를 갖춘 PC용 Red Dead Redemption 2는 PC의 성능을 최대로 활용하여 이 방대하고 풍부하며 생생한 세계의 구석구석에 생명을 불어넣었습니다. 늘어난 가시거리, 고품질의 글로벌 일루미네이션과 주변 폐색으로 개선된 낮과 밤의 조명, 반사 효과 향상, 어떤 거리에서도 더 깊이 있고 높은 해상도의 그림자, 테셀레이션 나무 텍스처, 향상된 잔디와 모피 텍스처 등이 모든 식물과 동물에 사실감을 불어넣습니다.
+
+PC용 Red Dead Redemption 2는 HDR을 지원하며, 4K 이상의 해상도나 다중 모니터 설정, 와이드 스크린 설정, 고속 주사율 등 하이엔드 디스플레이 설정에서 실행할 수 있는 기능을 제공합니다.
+",
+" 오픈 월드 풍부한 스토리 어드벤처 웨스턴 액션 멀티플레이어 현실적 슈팅 싱글 플레이어 분위기 있는 3인칭 슈팅 말 아름다운 성인 웅장한 사운드트랙 3인칭 샌드박스 고어 1인칭 1인칭 슈팅",
+"2019-12-5","레드 데드 리뎀션2","Red Dead Redemption 2",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 - April 2018 Update (v1803) 프로세서: Intel® Core™ i7-4770K / AMD Ryzen 5 1500X 메모리: 12 GB RAM 그래픽: Nvidia GeForce GTX 1060 6GB / AMD Radeon RX 480 4GB 네트워크: 초고속 인터넷 연결 저장공간: 150 GB 사용 가능 공간 사운드카드: Direct X Compatible",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 - April 2018 Update (v1803) 프로세서: Intel® Core™ i7-4770K / AMD Ryzen 5 1500X 메모리: 12 GB RAM 그래픽: Nvidia GeForce GTX 1060 6GB / AMD Radeon RX 480 4GB 네트워크: 초고속 인터넷 연결 저장공간: 150 GB 사용 가능 공간 사운드카드: Direct X Compatible",66000,25,26);
+
+
+insert into game(GMAE_INFO,CODE_TYPE,PUBLSH_DATE,KOR_NAME,ENG_NAME,MIN_SPEC,SEPC,DEFAULT_PRICE,DEVELOPER,PUBLISHER) values("
+
+게임에 대해
+사이버펑크 2077은 나이트 시티의 메갈로폴리스에서 펼쳐지는 오픈월드 액션 어드벤처 RPG입니다. 여러분은 사이버펑크 용병이 되어 생존을 위해 목숨을 걸고 싸워야 합니다. 개선 사항이 적용되고 새로운 무료 콘텐츠가 추가된 어두운 미래에서, 임무를 완수하여 평판을 쌓고 업그레이드를 잠금 해제하여 나만의 캐릭터와 플레이 스타일을 만들어 보세요. 여러분이 만들어나가는 관계와 선택에 따라, 여러분을 둘러싼 세계가 변화할 것입니다. 나이트 시티는 전설이 태어나는 곳입니다. 여러분은 어떤 전설을 써 내려가시겠습니까?나만의 사이버펑크를 찾아서사이버네틱 개조로 무장한 도시의 무법자가 되어 나이트 시티의 새로운 전설을 써 보세요.미래의 도시를 누비기나이트 시티엔 해야 할 일, 봐야 할 곳, 만나야 할 사람이 가득합니다. 언제, 어디를, 어떻게 갈지는 여러분에게 달려 있죠.전설이 되어라위험천만한 모험 속에서 여러분의 선택에 따라 운명이 결정되는 매력적인 캐릭터들을 만나 보세요.개선 사항으로 무장게임플레이와 경제, 도시, 지도 등 다양한 변경 사항과 개선 사항이 적용된 사이버펑크 2077을 경험해 보세요.무료 추가 콘텐츠 포함새로운 총기와 근접 무기, 추가 커스터마이즈 옵션 등 다양한 무료 아이템을 손에 넣으세요.GO TO CYBERPUNK.NET 
+",
+" 사이버펑크 오픈 월드 RPG 누드 싱글 플레이어 공상과학 미래적 1인칭 슈팅 1인칭 풍부한 스토리 성인 분위기 있는 탐험 액션 폭력 웅장한 사운드트랙 액션 RPG 어드벤처 캐릭터 커스터마이즈 몰입형 시뮬레이션",
+"2020-12-9","사이버펑크 2077","Cyberpunk 2077",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 프로세서: Intel Core i5-3570K or AMD FX-8310 메모리: 8 GB RAM 그래픽: NVIDIA GeForce GTX 970 or AMD Radeon RX 470 DirectX: 버전 12 저장공간: 70 GB 사용 가능 공간 추가 사항: In this game you will encounter a variety of visual effects that may provide seizures or loss of consciousness in a minority of people. If you or someone you know experiences any of the above symptoms while playing, stop and seek medical attention immediately.",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 프로세서: Intel Core i7-4790 or AMD Ryzen 3 3200G 메모리: 12 GB RAM 그래픽: GTX 1060 6GB / GTX 1660 Super or Radeon RX 590 DirectX: 버전 12 저장공간: 70 GB 사용 가능 공간 추가 사항: SSD recommended",66000,35,52);
+
+
+insert into game(GMAE_INFO,CODE_TYPE,PUBLSH_DATE,KOR_NAME,ENG_NAME,MIN_SPEC,SEPC,DEFAULT_PRICE,DEVELOPER,PUBLISHER) values("
+
+게임에 대해
+The World’s GameEA SPORTS™ FIFA 23을 통해 세계의 게임을 플레이하고 즐기세요. 한 차원 더 사실적인 게임플레이를 제공하는 HyperMotion2 기술을 경험하고, 출시 후 업데이트되는 남자 및 여자 FIFA World Cup™에 참가하고, 여자 클럽 팀으로 플레이하고, 크로스 플레이 기능*을 이용하는 등 다양한 재미를 즐기세요. FIFA 23에서 19,000명 이상의 선수, 700개 이상의 팀, 100여 개의 경기장 및 30개 이상의 리그를 통해 완벽하게 생생한 현실감을 느껴보세요.이 게임은 게임 내에서 사용되는 가상 화폐를 게임 내 구매 및 이용할 수 있으며, 이를 통해 확률형 아이템을 포함한 게임 내 가상 아이템을 구매할 수 있습니다.
+",
+" 스포츠 축구 몰입형 시뮬레이션 미식축구 경영 e스포츠 PvP 컨트롤러 3인칭 팀 기반 시뮬레이션 현실적 협동 로컬 협동 로컬 멀티플레이어 멀티플레이어 온라인 협동 싱글 플레이어 PvE 애니메이션",
+"2022-9-29","EA SPORTS 피파 23","EA SPORTS FIFA 23",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 64-bit 프로세서: Intel Core i5 6600k or AMD Ryzen 5 1600 메모리: 8 GB RAM 그래픽: NVIDIA GeForce GTX 1050 Ti or AMD Radeon RX 570 DirectX: 버전 12 네트워크: 초고속 인터넷 연결 저장공간: 100 GB 사용 가능 공간",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 64-bit 프로세서: Intel Core i7 6700 or AMD Ryzen 7 2700X 메모리: 12 GB RAM 그래픽: NVIDIA GeForce GTX 1660 or AMD Radeon RX 5600 XT DirectX: 버전 12 네트워크: 초고속 인터넷 연결 저장공간: 100 GB 사용 가능 공간",77000,11,12);
+
+
+insert into game(GMAE_INFO,CODE_TYPE,PUBLSH_DATE,KOR_NAME,ENG_NAME,MIN_SPEC,SEPC,DEFAULT_PRICE,DEVELOPER,PUBLISHER) values("
+
+게임에 대해
+아틀러스가 선사하는 쥬브나일 RPG 시리즈의 최신작 『P5R』이 또다시 등장.주인공은 어떤 사정으로 '도쿄'의 고등학교로 전학하게 된 소년.낮에는 극히 평범한 고등학생, 방과 후에는 부패한 어른들의 왜곡된 욕망을 훔치는 괴도로 암약.개성 넘치는 동료들과 함께하는 '세상 바로잡기'의 행보는 과연…? 
+",
+" JRPG 애니메이션 풍부한 스토리 RPG 턴제 전투 파티 기반 RPG 웅장한 사운드트랙 싱글 플레이어 턴제 전략 감정적인 복수 결말 다채로운 비주얼 노벨 연애 시뮬레이션 어드벤처 선정적인 내용 강도 사운드트랙 수사관 폭력",
+"2022-10-20","페르소나5 로얄","페르소나 5 더 로열",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 프로세서: Intel Core i7-4790, 3.4 GHz | AMD Ryzen 5 1500X, 3.5 GHz 메모리: 8 GB RAM 그래픽: Nvidia GeForce GTX 650 Ti, 2 GB | AMD Radeon R7 360, 2 GB DirectX: 버전 11 저장공간: 41 GB 사용 가능 공간 추가 사항: Low 720p @ 60 FPS. Requires a CPU which supports the AVX and SSE4.2 instruction set.",
+" 64비트 프로세서와 운영 체제가 필요합니다 운영체제: Windows 10 프로세서: Intel Core i7-4790, 3.4 GHz | AMD Ryzen 5 1500X 3.5 Ghz 메모리: 8 GB RAM 그래픽: Nvidia GeForce GTX 760, 2 GB | AMD Radeon HD 7870, 2 GB DirectX: 버전 11 저장공간: 41 GB 사용 가능 공간 추가 사항: High 1080p @ 60 FPS",69800,53,43);
+
+
