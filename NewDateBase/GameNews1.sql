efdd996d39607ba83288cc279b8d157b1a83d88c
ghostdog <87572532+ghostclog@users.noreply.github.com>
2022-12-04 21:23:24 +0900

    Add files via upload
    할인 일정 및 게임 뉴스 데이터 파일 위치 수정

---
+++ NewDateBase/GameNews1.sql
@@ -0,0 +1,81 @@
+insert into News(headline,news_url,news_time) values("
+카카오 배그, '비켄디 리본' 업데이트 사전 페이지 오픈
+","https://www.inven.co.kr/webzine/news/?news=279278","2022-11-29 14:14");
+insert into News(headline,news_url,news_time) values("
+[정보] 앞으로 진행될 콘텐츠 및 대회는? 이터널 리턴 일정 정리
+","https://www.inven.co.kr/webzine/news/?news=279291","2022-11-29 18:01");
+insert into News(headline,news_url,news_time) values("
+넷마블문화재단, 게임콘서트 통해 국내 게임산업 비전 공유
+","https://www.inven.co.kr/webzine/news/?news=279276","2022-11-29 13:23");
+insert into News(headline,news_url,news_time) values("
+출국 전 롤 한판? 인천국제공항 '라이엇 아케이드' 오픈
+","https://www.inven.co.kr/webzine/news/?news=279270","2022-11-29 11:57");
+insert into News(headline,news_url,news_time) values("
+과기부, 메타버스 윤리원칙 발표
+","https://www.inven.co.kr/webzine/news/?news=279269","2022-11-29 11:48");
+insert into News(headline,news_url,news_time) values("
+9번째 확장팩 출격, 'WoW 용군단' 29일 정식 출시
+","https://www.inven.co.kr/webzine/news/?news=279262","2022-11-29 10:38");
+insert into News(headline,news_url,news_time) values("
+[e스포츠 동향] 롤드컵 우승자 출신 '토이즈' 마약 밀매로 징역형
+","https://www.inven.co.kr/webzine/news/?news=279245","2022-11-28 19:23");
+insert into News(headline,news_url,news_time) values("
+[오피셜] 영원한 T1맨 '페이커' 3년 재계약
+","https://www.inven.co.kr/webzine/news/?news=279257","2022-11-28 19:18");
+insert into News(headline,news_url,news_time) values("
+1주년 '블루 아카이브', OST 패키지 예판 돌입
+","https://www.inven.co.kr/webzine/news/?news=279252","2022-11-28 18:13");
+insert into News(headline,news_url,news_time) values("
+넷마블, 2022  ESG 보고서 발간
+","https://www.inven.co.kr/webzine/news/?news=279250","2022-11-28 17:54");
+insert into News(headline,news_url,news_time) values("
+유동수 의원 게임위 질타, 문체부장관 "전면 쇄신하겠다"
+","https://www.inven.co.kr/webzine/news/?news=279246","2022-11-28 17:21");
+insert into News(headline,news_url,news_time) values("
+'눈물을 마시는 새'의 텍스트는 어떻게 이미지가 되었나?
+","https://www.inven.co.kr/webzine/news/?news=279197","2022-11-25 18:24");
+insert into News(headline,news_url,news_time) values("
+[리뷰] 인스턴트처럼 간편하고 맛있는 액션 '이블 웨스트'
+","https://www.inven.co.kr/webzine/news/?news=279183","2022-11-25 17:31");
+insert into News(headline,news_url,news_time) values("
+[단평] 아픈 건 사람이지, 지구가 아니니까
+","https://www.inven.co.kr/webzine/news/?news=279145","2022-11-24 16:21");
+insert into News(headline,news_url,news_time) values("
+이터널리턴, 신규 캐릭터 '타지아' 공개
+","https://www.inven.co.kr/webzine/news/?news=279143","2022-11-24 15:42");
+insert into News(headline,news_url,news_time) values("
+넥슨 떠나는 트오세, IMC게임즈에서 스팀 서비스 진행
+","https://www.inven.co.kr/webzine/news/?news=279137","2022-11-24 15:05");
+insert into News(headline,news_url,news_time) values("
+포켓몬 스칼렛&바이올렛, 3일 만에 천만 장 팔렸다
+","https://www.inven.co.kr/webzine/news/?news=279115","2022-11-24 10:59");
+insert into News(headline,news_url,news_time) values("
+라이엇, 직원 50명 참여해 '경복궁' 청소 나섰다
+","https://www.inven.co.kr/webzine/news/?news=279118","2022-11-24 10:29");
+insert into News(headline,news_url,news_time) values("
+카카오게임즈 신작 '디스테라' 스팀 얼리 억세스 출시
+","https://www.inven.co.kr/webzine/news/?news=279116","2022-11-24 10:17");
+insert into News(headline,news_url,news_time) values("
+골든 조이스틱 어워드, 올해의 게임은 '엘든 링'
+","https://www.inven.co.kr/webzine/news/?news=279114","2022-11-24 09:05");
+insert into News(headline,news_url,news_time) values("
+무료 업데이트 '위쳐3' 차세대 버전, 무엇이 바뀌나
+","https://www.inven.co.kr/webzine/news/?news=279112","2022-11-24 08:02");
+insert into News(headline,news_url,news_time) values("
+'갓 오브 워 라그나로크', 첫 주에만 510만 장 팔렸다
+","https://www.inven.co.kr/webzine/news/?news=279111","2022-11-24 08:02");
+insert into News(headline,news_url,news_time) values("
+이상헌 의원 "게이머한테 좋은 콘분위 기능 강화, 통과돼야"
+","https://www.inven.co.kr/webzine/news/?news=279089","2022-11-23 15:17");
+insert into News(headline,news_url,news_time) values("
+콜옵 모던 워페어2 및 워존 2.0에 축구 스타 번들 추가
+","https://www.inven.co.kr/webzine/news/?news=279086","2022-11-23 14:47");
+insert into News(headline,news_url,news_time) values("
+GSOK "낮은 확률에 자율규제, 필요하다면 검토"
+","https://www.inven.co.kr/webzine/news/?news=279081","2022-11-23 14:13");
+insert into News(headline,news_url,news_time) values("
+EA 신작 '와일드하츠', 가라쿠리 시스템 소개 영상 공개
+","https://www.inven.co.kr/webzine/news/?news=279082","2022-11-23 13:58");
+insert into News(headline,news_url,news_time) values("
+[풍경기] '지스타 2022 시디즈 브랜드존', 성황리에 종료
+","https://www.inven.co.kr/webzine/news/?news=279014","2022-11-23 13:20");
---
+++ NewDateBase/sale1.sql
@@ -0,0 +1,3 @@
+insert into sale_info(sale_title,sale_start,sale_end) values("오늘은 찬찬영이 쏜다~","2022-11-30","2023-11-29");
+insert into sale_info(sale_title,sale_start,sale_end) values("프라이데이 세일","2022-11-22","2022-11-29");
+insert into sale_info(sale_title,sale_start,sale_end) values("스팀 겨울 세일","2022-12-22","2023-01-5");
