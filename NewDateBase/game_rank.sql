e9bf9039276ca3428ba53a78df27e797a4cd7acd
ghostdog <87572532+ghostclog@users.noreply.github.com>
2022-12-05 07:22:15 +0900

    Update game_rank.sql
    컬럼 추가에 따른 내용 수정

--- NewDateBase/game_rank.sql
+++ NewDateBase/game_rank.sql
@@ -1,14 +1,14 @@
--- delete from game_rank_list;
+-- 순위 테이블만 drop하고 재생성 한 뒤, 입력하면 됩니다.
 
-insert into game_rank values(578080,1);
-insert into game_rank values(108600,2);
-insert into game_rank values(1174180,3);
-insert into game_rank values(1091500,4); 
-insert into game_rank values(1811260,5);
-insert into game_rank values(1938090,6);
-insert into game_rank values(960170,7);
-insert into game_rank values(1687950,8);
-insert into game_rank values(1817070,9);
-insert into game_rank values(289070,10);
+insert into game_rank values(578080,1,'PUBG: BATTLEGROUNDS','https://cdn.cloudflare.steamstatic.com/steam/apps/578080/header.jpg?t=1667814365');
+insert into game_rank values(108600,2,'Project Zomboid','https://cdn.cloudflare.steamstatic.com/steam/apps/108600/header.jpg?t=1667832656');
+insert into game_rank values(1174180,3,'Red Dead Redemption 2','https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg?t=1656615305');
+insert into game_rank values(1091500,4,'Cyberpunk 2077','https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header_koreana.jpg?t=1663663573'); 
+insert into game_rank values(1811260,5,'EA SPORTS™ FIFA 23','https://cdn.cloudflare.steamstatic.com/steam/apps/1811260/header_alt_assets_0.jpg?t=1669311247');
+insert into game_rank values(1938090,6,'콜 오브 듀티: 모던 워페어 II | 워존 2.0','https://cdn.cloudflare.steamstatic.com/steam/apps/1938090/header_koreana.jpg?t=1668813907' );
+insert into game_rank values(960170,7,'DJMAX RESPECT V','https://cdn.cloudflare.steamstatic.com/steam/apps/960170/header.jpg?t=1666858912');
+insert into game_rank values(1687950,8,'페르소나 5 더 로열','https://cdn.cloudflare.steamstatic.com/steam/apps/1687950/header.jpg?t=1666698742');
+insert into game_rank values(1817070,9,'Marvel’s Spider-Man Remastered','https://cdn.cloudflare.steamstatic.com/steam/apps/1817070/header.jpg?t=1667406675');
+insert into game_rank values(289070,10,'Sid Meier’s Civilization VI','https://cdn.cloudflare.steamstatic.com/steam/apps/289070/header.jpg?t=1669055236');
 
 select * from game_rank;
