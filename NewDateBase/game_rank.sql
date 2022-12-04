fd6f37b95f8b4ee9feefa20fbe7409ae7dcacbce
chan-00 <a82316@naver.com>
2022-12-04 22:25:20 +0900

    DB sql 오류사항 수정

--- NewDateBase/game_rank.sql
+++ NewDateBase/game_rank.sql
@@ -1,14 +1,14 @@
 -- delete from game_rank_list;
 
-insert into game_rank_list values(1,578080);
-insert into game_rank_list values(2,108600);
-insert into game_rank_list values(3,1174180);
-insert into game_rank_list values(4,1091500); 
-insert into game_rank_list values(5,1811260);
-insert into game_rank_list values(6,1938090);
-insert into game_rank_list values(7,960170);
-insert into game_rank_list values(8,1687950);
-insert into game_rank_list values(9,1817070);
-insert into game_rank_list values(10,289070);
+insert into game_rank values(1,578080);
+insert into game_rank values(2,108600);
+insert into game_rank values(3,1174180);
+insert into game_rank values(4,1091500); 
+insert into game_rank values(5,1811260);
+insert into game_rank values(6,1938090);
+insert into game_rank values(7,960170);
+insert into game_rank values(8,1687950);
+insert into game_rank values(9,1817070);
+insert into game_rank values(10,289070);
 
 select * from game_rank_list;
